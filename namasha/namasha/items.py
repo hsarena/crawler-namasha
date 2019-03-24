2a20e903f538db9f1c7b34c3643f3a5e3a9be8e5
hsarena <hmd.salehian@gmail.com>
2019-03-24 05:02:49 +0430

    write spiders

--- namasha/namasha/items.py
+++ namasha/namasha/items.py
@@ -6,9 +6,9 @@
 # https://doc.scrapy.org/en/latest/topics/items.html
 
 import scrapy
+from scrapy.item import Item, Field
 
-
-class NamashaItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+class NamashaItem(Item):
+    category = Field()
+    title = Field()
+    link = Field()
--- namasha/namasha/spiders/videos.py
+++ namasha/namasha/spiders/videos.py
@@ -1,11 +1,31 @@
 # -*- coding: utf-8 -*-
 import scrapy
+from namasha.items import NamashaItem
 
 
 class VideosSpider(scrapy.Spider):
     name = 'videos'
     allowed_domains = ['www.namasha.com']
-    start_urls = ['http://www.namasha.com/']
+
+    categories = ['funny', 'sport', 'technology',
+                  'animation', 'educational', 'vehicle',
+                  'news', 'music', 'game',
+                  'animals', 'accidents', 'religious']
+
+    start_urls = ['https://www.namasha.com/playlist/{0}'.format(category) for category in categories]
+    custom_settings = {
+        'DEPTH_LIMIT': '1',
+    }
 
     def parse(self, response):
-        pass
+        items = []
+        category = response.xpath('//*[@id="content"]/div[1]/h1/text()').get()
+        videos = response.xpath('//*[@id="content"]/div[2]/div/div')
+        for video in videos:
+            item = NamashaItem()
+            item['category'] = category
+            item['title'] = video.xpath('div/a/text()').get()
+            item['link'] = videos.xpath('div/a/@href').get()
+            items.append(item)
+        print(items)
+        return items
